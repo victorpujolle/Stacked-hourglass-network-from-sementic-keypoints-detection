5395301c25ceb6baef071c6803b84cd1e29ed728
victor.pujolle <victor.pujolle@laposte.net>
2018-07-05 18:58:03 +0900

    suppression of gamma

--- hourglass_dann_v10.py
+++ hourglass_dann_v10.py
@@ -95,9 +95,9 @@ class HourglassModel:
             )
 
             # gamma implemantation
-            gamma = 0.1
-            hm_loss = (hm_loss - (1 - gamma) * (1 - tf.reshape(self.gtDomain, [4, 1, 1, 1, 1])) * hm_loss) * (1 + gamma) / (2 * gamma)
-            domain_loss = (domain_loss - (1 - gamma) * (1 - self.gtDomain) * domain_loss) * (1 + gamma) / (2 * gamma)
+            #gamma = 0.1
+            #hm_loss = (hm_loss - (1 - gamma) * (1 - tf.reshape(self.gtDomain, [4, 1, 1, 1, 1])) * hm_loss) * (1 + gamma) / (2 * gamma)
+            #domain_loss = (domain_loss - (1 - gamma) * (1 - self.gtDomain) * domain_loss) * (1 + gamma) / (2 * gamma)
 
             self.loss = tf.reduce_mean(hm_loss, name='cross_entropy_heatmap_loss') + tf.reduce_mean(domain_loss, name='cross_entropy_domain_loss')
 
