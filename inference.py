a596e661f17df88d00ae12d0a2aaa9acb78636a7
victor.pujolle <victor.pujolle@laposte.net>
2018-07-10 10:39:31 +0900

    new version

--- inference.py
+++ inference.py
@@ -152,9 +152,9 @@ class Inference():
 				
 
 if __name__ == '__main__':
-	infer=Inference(config_file = 'config.cfg', model = '../trained_networks/hg_test_33_50', yoloModel = 'YOLO_small.ckpt')
-	val_path='../datasets/val_book_masked'
-	result_path='../datasets/val_book_masked_result'
+	infer=Inference(config_file = 'config.cfg', model = '../trained_networks/hg_test_35_200', yoloModel = 'YOLO_small.ckpt')
+	val_path='../datasets/val_new'
+	result_path='../datasets/val_new_result'
 # 	val_path='datasets/val'
 # 	result_path='datasets/val_result'
 	val_list=os.listdir(val_path)
